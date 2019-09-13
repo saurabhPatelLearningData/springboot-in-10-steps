04efebaf3d6a10793226b2b2c91623f4b38783ec
saurabhPatel1712 <45526601+saurabhPatel1712@users.noreply.github.com>
2019-09-13 16:53:43 +0530

    Add files via upload

---
+++ springboot-in-10-steps/src/main/java/com/javaLearning/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,18 @@
+package com.javaLearning.springboot.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		
+		return Arrays.asList(new Book(1, "Spring Boot Basics", "Java"));
+	} 
+	
+}
